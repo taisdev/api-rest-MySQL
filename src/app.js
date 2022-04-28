71a378310671c19ffcf4bf04a33d115113985bed
Taís Oliveira <tsoliveira868@gmail.com>
2022-04-28 16:00:06 -0300

    Adicionado cors

--- src/app.js
+++ src/app.js
@@ -4,9 +4,26 @@ dotenv.config();
 
 import './database';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
+
+const whisteList = [
+  'http://34.95.143.41',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whisteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -16,6 +33,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
   }
